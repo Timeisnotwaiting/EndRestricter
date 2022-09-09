2e6afbd264f4dad8b07134245285694d6b83b0b9
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-09 23:03:52 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,9 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+
+MONGO_URL = os.getenv("MONGO_DB_URL", None)
+
+def build_mongo():
+    mongo = MongoClient(MONGO_URL)
+    return mongo.RES
+    
