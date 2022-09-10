4c22d7ebfb14559c4046e698d50a6c1aca2dd819
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-10 07:49:01 +0530

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -1,2 +1,18 @@
 from pyrogram import Client
 from pyrogram.types import Message
+from db import *
+
+
+async def negate_fn(_, m):
+    usage = f"<i>**Usage** : /negate_name [ NAME ] <i>"
+    if await is_blocked(m.from_user.id):
+        return
+    mem = await _.get_chat_member(m.chat.id, m.from_user.id)
+    if not mem.status in ["creator", "administrator"]:
+        return
+    if not len(m.command) == 2:
+        return await m.reply(f"<i>**Usage** : /negate_name [ NAME ] <i>")
+    name = m.text.split()[1]
+    if not name:
+        return await m.reply(usage)
+    
