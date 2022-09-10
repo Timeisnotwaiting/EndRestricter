d437670fc1453bf7426ea549812a9a9ed7f04fd6
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-10 11:45:15 +0530

    Update restrict.py

--- restrict.py
+++ restrict.py
@@ -6,3 +6,10 @@ API_HASH = os.getenv("API_HASH", None)
 BOT_TOKEN = os.getenv("BOT_TOKEN", None)
 
 app = Client(":environment:", API_ID, API_HASH, BOT_TOKEN)
+
+@app.on_message(filters.command(["negate_name", "denegate_name"]))
+async def appendix(_, m):
+    if m.text.split()[0][1].lower() == "d":
+        return await denegate_fn(_, m)
+    return await negate_fn(_, m)
+    
